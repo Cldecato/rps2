7f39ab2b13c8f09dfc45595c8b23ea7b723c990c
Corey Decato <Corey1@Coreys-MBP.lan>
2018-04-25 12:53:32 -0400

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    var wins = 0;
+    var losses = 0;
+    var ties = 0;
+    var user1Guess;
+    var user2Guess;
+
+    
+    //  Game Logic Here
+    //   if ((user1Guess === "r") || (user1Guess === "p") || (user1Guess === "s")) {
+    
+    //     if ((user1Guess === "r") && (user2Guess === "s")) {
+    //       wins++;
+    //     } else if ((user1Guess === "r") && (user2Guess === "p")) {
+    //       losses++;
+    //     } else if ((user1Guess === "s") && (user2Guess === "r")) {
+    //       losses++;
+    //     } else if ((user1Guess === "s") && (user2Guess === "p")) {
+    //       wins++;
+    //     } else if ((user1Guess === "p") && (user2Guess === "r")) {
+    //       wins++;
+    //     } else if ((user1Guess === "p") && (user2Guess === "s")) {
+    //       losses++;
+    //     } else if (user1Guess === user2Guess) {
+    //       ties++;
+    //     }
+    //   }
+})
