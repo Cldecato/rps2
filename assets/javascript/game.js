a5f6d88a0e62a7d75356f1c2f7c16944b88520f5
Corey Decato <Corey1@Coreys-MacBook-Pro.local>
2018-05-01 15:15:04 -0400

    commiting to submit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,43 @@ $(document).ready(function() {
     var wins = 0;
     var losses = 0;
     var ties = 0;
-    var user1Guess;
-    var user2Guess;
+    var username = "";
+    var userGuess = 'rock';
+    // var user2Guess = 'paper';
 
-    
+    var config = {
+        apiKey: "AIzaSyDrRos3pvaMEm_n-vRHZ4mpIm37cQIjk3U",
+        authDomain: "project1-c0437.firebaseapp.com",
+        databaseURL: "https://project1-c0437.firebaseio.com",
+        projectId: "project1-c0437",
+        storageBucket: "project1-c0437.appspot.com",
+        messagingSenderId: "657654491071"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+
+    $("#start").on("click", function(event) {
+        event.preventDefault();
+        var user = $("#username").val().trim();
+        $("#user1").text(user);
+        $("#name-input").empty();
+        $("#name-input").text("Welcome " + user + " you are Player 1");
+        database.ref().set({
+            name: user,
+            wins: wins,
+            losses: losses,
+            ties: ties,
+            guess: userGuess
+        });
+    })
+
+    $("#send").on("click", function(event) {
+        event.preventDefault();
+        var message = $("#chat").val().trim();
+        $("#chat-box").append(message);
+    })
+
+
     //  Game Logic Here
     //   if ((user1Guess === "r") || (user1Guess === "p") || (user1Guess === "s")) {
     
